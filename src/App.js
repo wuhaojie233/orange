1520794dade5a41b30e10c187cddb3ee98ec1f51
haha <33541346+wuhaojie233@users.noreply.github.com>
2020-11-22 22:51:50 +0800

    hello world!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App">Hello world!</div>;
 }
 
 export default App;
