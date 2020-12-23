b8f7be3e59b38cfa34fe3c417eb607a4f55521eb
haha <33541346+wuhaojie233@users.noreply.github.com>
2020-12-24 00:05:37 +0800

    auth login

---
+++ src/hooks/auth/auth-login.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+import { Redirect } from 'react-router-dom'
+
+const AuthLogin = (viewComponent: FC) => {
+    return viewComponent
+//   return () => {
+//     return <Redirect to="/login"></Redirect>
+//   }
+}
+
+export default AuthLogin
--- src/layout/home/home.tsx
+++ src/layout/home/home.tsx
@@ -1,35 +1,36 @@
-import { Breadcrumb, Layout } from "antd";
+import { Breadcrumb, Layout } from 'antd'
 import React, { FC } from 'react'
-import Headers from '../headers/headers';
-import Sidebar from '../sidebar/sidebar';
-import { renderRoutes } from 'react-router-config';
+import Headers from '../headers/headers'
+import Sidebar from '../sidebar/sidebar'
+import { renderRoutes } from 'react-router-config'
+import AuthLogin from '../../hooks/auth/auth-login'
 const { Content } = Layout
 const Home: FC = (props: any) => {
-    return (
-        <Layout className="m-layout">
-            <Headers></Headers>
-            <Layout>
-                <Sidebar></Sidebar>
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <Content
-                        className="site-layout-background"
-                        style={{
-                            padding: 24,
-                            margin: 0,
-                            minHeight: 280,
-                        }}
-                    >
-                        {renderRoutes(props.route.routes)}
-                    </Content>
-                </Layout>
-            </Layout>
+  return (
+    <Layout className="m-layout">
+      <Headers></Headers>
+      <Layout>
+        <Sidebar></Sidebar>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            {renderRoutes(props.route.routes)}
+          </Content>
         </Layout>
-    )
+      </Layout>
+    </Layout>
+  )
 }
 
 export default Home
