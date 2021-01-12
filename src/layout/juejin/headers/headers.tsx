b44d51bb63b67fdad52e51a2262161cabe393f20
haha <33541346+wuhaojie233@users.noreply.github.com>
2021-01-12 18:15:00 +0800

    search

--- src/layout/juejin/headers/headers.tsx
+++ src/layout/juejin/headers/headers.tsx
@@ -1,25 +1,36 @@
 import React, { useState } from 'react'
-import { Menu, Layout, Input, Button } from 'antd';
+import { Menu, Layout, Input, Dropdown } from 'antd';
 // const { SubMenu } = Menu;
 import './headers.less'
 import PCLogo from '@asstes/juejin/header/pc-logo.svg'
 import MobileLogo from '@asstes/juejin/header/mb-logo.svg'
-import { BellOutlined } from '@ant-design/icons';
+import { BellOutlined, SearchOutlined } from '@ant-design/icons';
 const { Header } = Layout
 const { Search } = Input;
+const menuList = [
+    { title: '首页', key: 'menu-1', active: 'one' },
+    { title: '沸点', key: 'menu-2', active: 'two' },
+    { title: '小册', key: 'menu-3', active: 'three' },
+    { title: '活动', key: 'menu-4', active: 'four' },
+]
+
 const Headers = () => {
     const isMobile = false
-    const menuList = [
-        { title: '首页', key: 'menu-1', active: 'one' },
-        { title: '沸点', key: 'menu-2', active: 'two' },
-        { title: '小册', key: 'menu-3', active: 'three' },
-        { title: '活动', key: 'menu-4', active: 'four' },
-    ]
+
     const [state, setState] = useState('menu-1')
     const handleClick = (event: any) => {
         setState(event.key);
     }
     const onSearch = (value: any) => console.log(value);
+    const handleMenuClick = () => {
+
+    }
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item key="1">发布沸点</Menu.Item>
+        </Menu>
+    );
+
     return <Header>
         <div className="header-wrapper">
             <Menu onClick={handleClick} selectedKeys={[state]} mode="horizontal">
@@ -45,10 +56,15 @@ const Headers = () => {
 
             </Menu>
             <div className="h-search">
-                <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+                <Input
+                    className="search-text"
+                    placeholder="搜索掘金"
+                    suffix={
+                        <SearchOutlined />
+                    } />
             </div>
             <div className="h-add">
-                <Button type="primary">写文章</Button>
+                <Dropdown.Button overlay={menu}>Actions</Dropdown.Button>
             </div>
             <div className="m-notification">
                 <BellOutlined />
@@ -60,4 +76,5 @@ const Headers = () => {
     </Header>
 
 }
+
 export default Headers
