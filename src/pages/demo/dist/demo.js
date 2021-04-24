7372a86b05b63c2af8f831d6c4a1d4b7f9f203f8
haha <33541346+wuhaojie233@users.noreply.github.com>
2021-04-24 12:16:11 +0800

    更改冲突

--- src/pages/demo/dist/demo.js
+++ src/pages/demo/dist/demo.js
@@ -1,24 +1,39 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 exports.__esModule = true;
 var react_1 = require("react");
 var antd_1 = require("antd");
-// study 8(开始)
-var DemoReducer = function (state, action) {
-    switch (action.type) {
-        case 'increment':
-            return state + 1;
-        case 'decrement':
-            return state - 1;
-        default:
-            return state;
-    }
-};
-var Demo = function () {
+var demoGroupState_ts_1 = require("storeDemo/demoGroupState.ts");
+var modal_1 = require("./modal");
+var demoAction_1 = require("./demoAction");
+// study 16(3/2)
+var Demo = function (props) {
     var inputELement = react_1.useRef();
     var focus = function () {
         inputELement.current.focus();
     };
-    var _a = react_1.useReducer(DemoReducer, 1), state = _a[0], dispatch = _a[1];
+    /**
+     * React Hooks
+     */
+    var _a = react_1.useState(function () {
+        return props.one || { age: 0 };
+    }), state = _a[0], setState = _a[1];
+    var handleAge = function () {
+        setState(function (state) {
+            return __assign(__assign({}, state), { age: state.age + 10 });
+        });
+    };
+    // const [state, dispatch] = useReducer(DemoReducer, 1)
     //   useeffect
     //   useEffect(() => {
     //     console.log('挂载之后,数据发生更新之后')
@@ -32,8 +47,19 @@ var Demo = function () {
         react_1["default"].createElement("h3", null, "useReducer:"),
         react_1["default"].createElement("span", null,
             "reducerState:",
-            state),
-        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return dispatch({ type: 'increment' }); } }, "increment"),
-        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return dispatch({ type: 'decrement' }); } }, "decrement")));
+            props.count),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return props.increment(10); } }, "increment"),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return props.increment_async(100); } }, "increment_async"),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return props.increment_saga_async(1000); } }, "increment_async_saga"),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return props.decrement(5); } }, "decrement"),
+        react_1["default"].createElement("hr", null),
+        react_1["default"].createElement("h3", null, "modal"),
+        react_1["default"].createElement(modal_1["default"], null),
+        react_1["default"].createElement(demoAction_1["default"], null),
+        react_1["default"].createElement("hr", null),
+        react_1["default"].createElement("h3", null, "React Hooks"),
+        react_1["default"].createElement("span", null, state.age),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return setState(__assign(__assign({}, state), { age: state.age + 10 })); } }, "+ 10"),
+        react_1["default"].createElement(antd_1.Button, { type: "primary", onClick: function () { return handleAge(); } }, "handleAge")));
 };
-exports["default"] = Demo;
+exports["default"] = demoGroupState_ts_1["default"](Demo, { reducer: 'demo', states: ['demo'] });
